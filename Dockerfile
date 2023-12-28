07af25b718cdb5d2dd5a24f9a4d53c1260554626
Sam Sonter <62767142+asapsonter@users.noreply.github.com>
2023-12-28 13:19:02 +0800

    jquery set up bug fix 2023-12-28

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,29 @@
 
-FROM nginx:stable
+# Use an official Node.js runtime as the base image
+FROM node:14
+
+# Set the working directory in the Docker image
+WORKDIR /usr/src/app
+
+# Copy package.json and package-lock.json into the Docker image
+COPY package*.json ./
+
+# Install the dependencies in the Docker image
+RUN npm install
+
+# Copy the rest of your code into the Docker image
+COPY . .
+
+# Expose port 8080
+EXPOSE 8080
+
+# Start the application
+CMD [ "node", "server.js" ]
+
+# FROM nginx:stable
 
 
-COPY . /usr/share/nginx/html
+# COPY . /usr/share/nginx/html
 
-EXPOSE 80
-CMD ["nginx", "-g", "daemon off;"]
+# EXPOSE 80
+# CMD ["nginx", "-g", "daemon off;"]
--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,31 @@
-const items = document.querySelector('.button');
-button.animate([
-    { transform: 'translateX(0px)' },
-    { transform: 'translateX(20px)' }
-    ], {
-    duration: 2000,
-    easing: 'ease-in-out',
-    direction: 'alternate',
-    iterations: Infinity
-    });
+// var jsdom = require("jsdom");
+// var $ = require("jquery")(new jsdom.JSDOM().window);
+
+// const items = document.querySelector(".button");
+// button.animate(
+//   [{ transform: "translateX(0px)" }, { transform: "translateX(20px)" }],
+//   {
+//     duration: 2000,
+//     easing: "ease-in-out",
+//     direction: "alternate",
+//     iterations: Infinity,
+//   }
+// );
+
+$(function () {
+  var currentDate = new Date().getFullYear();
+  $("#copyRight").html("Copyright © " + currentDate + " Fireship LLC <br>");
+});
+window.onload = function () {
+  if (window.jQuery) {
+    // jQuery is loaded
+    console.log("jQuery has loaded!");
+  } else {
+    // jQuery is not loaded
+    console.log("jQuery has not loaded!");
+  }
+};
+
+$(document).onload = function () {
+  alert("jQuery has loaded!");
+};
